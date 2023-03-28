61b6acf1d7d1b2c1253924636a2790532a4fff40
GeekMonstar <94204578+GeekMonstar@users.noreply.github.com>
2023-03-28 04:07:55 +0200

    init

---
+++ app.js
@@ -0,0 +1,81 @@
+const body = document.body;
+const colors = document.querySelectorAll('.color');
+const range = document.querySelector('#angle');
+const labels = document.querySelectorAll('label');
+const angleLabel = document.querySelector('.angle-value');
+const randomBtn = document.querySelector('.random-btn');
+const copyBtn = document.querySelector('.copy-btn');
+let state = {
+    color1: '#FF0000',
+    color2: '#006EFF',
+    angle: 90
+}
+
+start();
+generateHex();
+
+for (let i = 0; i < colors.length; i++){
+    colors[i].addEventListener('input', e => {handleColorChange(e, labels[i])});
+}
+
+range.addEventListener('input', handleAngleChange);
+
+randomBtn.addEventListener('click', handleRandom);
+copyBtn.addEventListener('click', handleCopy);
+
+function handleColorChange(event, element){
+    element.textContent = event.target.value;
+    update();
+}
+
+function handleAngleChange(event) {
+    angleLabel.textContent = `${event.target.value}°`;
+    update();
+}
+
+function handleRandom() {
+    const newColors = generateHex();
+    colors[0].value = newColors[0];
+    colors[1].value = newColors[1];
+    update();
+}
+
+function handleCopy() {
+    const { color1, color2, angle } = state;
+    const gradient = `background: linear-gradient(${angle}deg, ${color1}, ${color2})`;
+    navigator.clipboard.writeText(gradient);
+}
+
+function start() {
+    const { color1, color2, angle } = state;
+    colors[0].value = state.color1;
+    colors[1].value = state.color2;
+    labels[0].textContent = state.color1;
+    labels[1].textContent = state.color2;
+    range.value = state.angle;
+    angleLabel.textContent = state.angle;
+    body.style.background = `linear-gradient(${angle}deg, ${color1}, ${color2})`;
+}
+
+function update() {
+    state = {
+        color1: colors[0].value,
+        color2: colors[1].value,
+        angle: parseInt(range.value)
+    }
+    const { color1, color2, angle } = state;
+    body.style.background = `linear-gradient(${angle}deg, ${color1}, ${color2})`;
+}
+
+function generateHex() {
+    const charString = '0123456789ABCDEF';
+    const hexArray = ['#', '#'];
+
+    for (let i = 0; i < hexArray.length; i++){
+        for (let j = 0; j < 6; j++){
+            const randomIndex = Math.trunc(Math.random() * 16);
+            hexArray[i] += charString[randomIndex];
+        }
+    }
+    return hexArray
+}
